347856fa8ef611802859c36100fd9669deb8ae7d
Ev Blandin <eblandin002@iutbayonne.iut-pau.fr>
2020-11-27 16:53:57 +0100

    Changement de la route Accueil

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -9,12 +9,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProStageController extends AbstractController
 {
     /**
-     * @Route("/pro/stage", name="pro_stage")
+     * @Route("/", name="prostage_accueil")
      */
     public function index(): Response
     {
         return $this->render('pro_stage/index.html.twig', [
-            'controller_name' => 'ProStageController',
+            'controller_name' => 'Contrôleur ProStage',
         ]);
     }
 }
