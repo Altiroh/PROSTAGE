94465414537f8098b7a3d3df2822ccf67a45dd2b
Ev Blandin <eblandin002@iutbayonne.iut-pau.fr>
2021-01-15 19:59:52 +0100

    Toute les pages sont intégrées avec les entités

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Stage;
+use App\Entity\Formation;
+use App\Entity\Entreprise;
 
 class ProStageController extends AbstractController
 {
@@ -25,7 +27,15 @@ class ProStageController extends AbstractController
      */
     public function filtrage(): Response
     {
-        return $this->render('pro_stage/filtrage.html.twig');
+      //Récuperer le répertoire de l'entité formation
+      $repForm = $this->getDoctrine()->getRepository(Formation::class);
+      //Récuperer ce qui a été saved dans la BD
+      $formations = $repForm->findAll();
+      //Récuperer le répertoire de l'entité entreprise
+      $repEtp = $this->getDoctrine()->getRepository(Entreprise::class);
+      //Récuperer ce qui a été saved dans la BD
+      $entreprises = $repEtp->findAll();
+    return $this->render('pro_stage/filtrage.html.twig',['formations'=>$formations,'entreprises'=>$entreprises]);
     }
     /**
      * @Route("/", name="prostage_accueil")
@@ -40,6 +50,28 @@ class ProStageController extends AbstractController
 
         //Envoyer les ressources récupérer à la vue chargée pour les affichers
         return $this->render('pro_stage/accueil.html.twig',['stages'=>$stages]);
+    }
+    /**
+     * @Route("/formation-{nom}", name="prostage_selectForm")
+     */
+    public function selectForm($nom): Response
+    {
+      //Récuperer le répertoire de l'entité stage
+      $repStage = $this->getDoctrine()->getRepository(Stage::class);
+      //Récuperer ce qui a été saved dans la BD
+      $stage = $repStage->find($nom);
+      return $this->render('pro_stage/stageSelect.html.twig',['stages' => $stages]);
+    }
+    /**
+     * @Route("/enterprise-{nom}", name="prostage_selectForm")
+     */
+    public function selectForm($nom): Response
+    {
+      //Récuperer le répertoire de l'entité stage
+      $repStage = $this->getDoctrine()->getRepository(Stage::class);
+      //Récuperer ce qui a été saved dans la BD
+      $stage = $repStage->find($nom);
+      return $this->render('pro_stage/stageSelect.html.twig',['stages' => $stages]);
     }
 
 }
